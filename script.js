ab7a9dfeb115c59efd86035ad449f3c1b5bff4af
Abhinand KR <nanduttanvsabhi@gmail.com>
2022-12-01 11:57:09 +0530

    Add horizontal and vertical grid using div

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+for(let i=0; i< 16; i++)
+{
+    var div = document.createElement("div");
+    div.classList.add("h-div")
+    document.getElementById("horizontal").appendChild(div)
+     
+}
+
+
+for(let i=0; i<16; i++)
+{
+    var div = document.createElement("div");
+    div.classList.add("v-div")
+    document.getElementById("vertical").appendChild(div);
+}
